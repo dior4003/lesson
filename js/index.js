04e96bf0ad31003cbe42acdc5dced5c06b0b858e
dior4003 <diyor3933@gmail.com>
2022-10-27 13:55:28 +0000

    started corusel

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
 let list = document.querySelector("#list");
 let button = document.querySelector("#btn");
+let img = document.querySelector(".corusel_item");
+let left_btn = document.querySelector(".left_btn");
+let right_btn = document.querySelector(".right_btn");
+let corusel_item = document.querySelectorAll(".corusel_item");
+let images= ["https://uzpesni.ru/wp-content/uploads/2021/07/Munisa-Rizayeva-Moon-Band-Qaytmaydi-kech-mp3-image-1024x552.jpg", 
+"https://i.ytimg.com/vi/FAg8JZwFM2U/maxresdefault.jpg",
+"https://i.ytimg.com/vi/h3VMXz-LsGs/maxresdefault.jpg",
+"https://i.pinimg.com/736x/18/bd/41/18bd4134927a899db79cb9ea64643838.jpg"]
 
 button.addEventListener("click" , ()=>{
-    console.log("hello");
+    if(list.classList[1]==="toggle_list"){
+        list.classList.remove("toggle_list")
+    }else{
+        list.classList.add("toggle_list")
+    }
 })
 
